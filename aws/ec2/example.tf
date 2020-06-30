c8616856c5cc2a9d5cbce17a0988f75ee577f4c3
Marton Soos <marton.soos@secrethub.io>
2020-06-30 17:03:51 +0200

    Remove unused ingress rule

--- aws/ec2/example.tf
+++ aws/ec2/example.tf
@@ -90,13 +90,6 @@ resource "aws_security_group" "secrethub_demo" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = "22"
-    to_port     = "22"
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
