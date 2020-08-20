0b32b98ea429e00ae1cacefabb9ff14abeec3bed
Horia Culea <horiaculea@gmail.com>
2020-08-20 18:30:47 +0300

    added back files

---
+++ application/kitura/HelloKitura/Sources/HelloKitura/main.swift
@@ -0,0 +1,19 @@
+import Kitura
+import Foundation
+
+let router = Router()
+
+if ProcessInfo.processInfo.environment["DEMO_USERNAME"] == nil || ProcessInfo.processInfo.environment["DEMO_PASSWORD"] == nil {
+    router.get("/") { request, response, next in
+        response.send("either your username, your password, or both have not been set")
+        next()
+    }
+}
+else {
+    router.get("/") { request, response, next in
+        response.send("welcome, \(ProcessInfo.processInfo.environment["DEMO_USERNAME"] ?? "")")
+    }
+}
+
+Kitura.addHTTPServer(onPort: 8080, with: router)
+Kitura.run()
