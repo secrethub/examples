b7644175ba45a2e3990e1442f4e8c12bebebc124
Floris van der Grinten <floris@grinten.com>
2020-09-17 11:31:28 +0200

    Use Terraform provider registry

--- google-cloud/gce/main.tf
+++ google-cloud/gce/main.tf
@@ -2,7 +2,14 @@ provider "google" {
   project = var.gcp_project_id
 }
 
-provider "secrethub" {}
+terraform {
+  required_providers {
+    secrethub = {
+      source = "secrethub/secrethub"
+      version = ">= 1.2.3"
+    }
+  }
+}
 
 resource "google_kms_key_ring" "secrethub" {
   name     = "secrethub"
