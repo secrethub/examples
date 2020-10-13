0c4030b0d02d060e4fac8164dda87f4f11c61a34
Horia Culea <45151601+hculea@users.noreply.github.com>
2020-10-13 11:23:37 +0300

    typo, changed to not all variables have been set

--- application/phoenix/hello_phoenix_api/lib/hello_phoenix_api_web/views/hello_view.ex
+++ application/phoenix/hello_phoenix_api/lib/hello_phoenix_api_web/views/hello_view.ex
@@ -5,7 +5,7 @@ defmodule HelloPhoenixApi.HelloView do
       if System.get_env("DEMO_USERNAME") != nil && System.get_env("DEMO_PASSWORD") != nil do
         %{message: "Hello, " <> System.get_env("DEMO_USERNAME") <> "!"}
       else 
-        %{message: "all the variables have been set"}
+        %{message: "not all the variables have been set"}
       end
     end
   end
